b23e19959be229f9010f3bb9db336acff3302cc3
Abilio Almeida Eiroa <bili@nextail.co>
2022-02-08 20:36:40 +0100

    Separating integrationa and unit tests

--- __tests__/unit/routes/index.spec.js
+++ __tests__/unit/routes/index.spec.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 
 import { screen, render } from '@testing-library/svelte';
 
-import Index from '../../src/routes/index.svelte';
+import Index from '../../../src/routes/index.svelte';
 
 describe('testing jest', () => {
   fit('should render the title', () => {
